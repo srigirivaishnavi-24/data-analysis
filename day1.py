93546234572eb64d72d2d5e3ff097201962a0fee
srigirivaishnavi-24 <srigirivaishnavi@gmail.com>
2026-05-30 20:19:51 +0530

    Add files via upload
    🚀 Day 15/undefined of my Data Analysis Bootcamp with @UnifyLabs!
    
    Today I learned: Variables In Python
    
    Consistently building my skills and actively working on hands-on practical applications! 💪
    
    #DataAnalysis #UnifyLabs #CodingBootcamp #Day15 #LearnToCode

---
+++ day1.py
@@ -0,0 +1,36 @@
+#input and output
+print("hello world")
+print(23)
+print("coding")
+
+#variable
+#variable: A variable is a name given to a memory location in a program
+name="ram"
+age=34
+height=6.1
+print(f"my name is {name}.")
+print(f"my age is {age} and my height is {height}")
+
+#data types
+a="kumar"
+b=67
+c=1.2
+print(type(a))
+print(type(b))
+print(type(c))
+
+a=23
+b=56
+print(a+b)
+c=45
+print(a+c)
+
+a=5
+b=8
+txt="@"
+print(a*txt*b)
+
+a,b="2",3
+txt="@"
+print((a+txt)*b)
+
